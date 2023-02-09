0a36f1c3a5fa962f4fc243bf57a0b88845bbf73e
Polina Margalnikova <polina.margalnikova@tktk.ee>
2023-02-09 13:34:24 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+//rakendus kusib kasutajal sisestada tema nime
+//rakendus terviab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input (koik mis meie sisestame)
+
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //output
